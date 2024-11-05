961e0f489f73d149cb59914385cb56a91e50ccf2
Carlos Gabriel Hopital <ghopital@hotmail.com>
2024-11-05 20:11:58 +0100

    Add Building Blocks project

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/BadRequestException.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.Exceptions;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message) : base(message)
+    { }
+
+    public BadRequestException(string name, object key) : base($"Entity \"{name}\" ({key}) was not found")
+    { }
+}
